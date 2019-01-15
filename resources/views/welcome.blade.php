0be303365e3c5b9a5b468f80d84467b14ab45b81
baohoangfc <baohoangfc@gmail.com>
2019-01-16 00:22:43 +0700

    create app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,30 +68,19 @@
         </style>
     </head>
     <body>
-            <div id="bao">
-                <?php 
-                    //$i = 10;
-                     
-                    // function Fibo($i){
-                    //    if($i<=2){
-                    //        return 1;
-                    //    } else {
-                    //        return (Fibo($i-2) + Fibo($i-1));
-                    //    }
-                    // }
-
-                    // echo Fibo(12);
-                    // echo 'Dữ Liệu Chúng Tôi Nhận Được Là <br/>';
-                    // foreach ($_GET as $key => $val)
-                    // {
-                    //     echo '<strong>' . $key . ' => ' . $val . '</strong><br/>';
-                    // }
-                    <form method="POST">
-                        Username: <input type="text" name="username" value=""/> <br/>
-                        password: <input type="password" name="password" value=""/><br/>
-                        <input type="submit" name="form_click" value="Gửi Dữ Liệu"/>
-                    </form>
-                ?>
+            <div id="bao">   
+                 <form method="POST">
+                    Username: <input type="text" name="username" value=""/> <br/>
+                    password: <input type="password" name="password" value=""/><br/>
+                    <input type="submit" name="form_click" value="Gửi Dữ Liệu"/>
+                    <?php 
+                        if (isset($_POST['form_click'])){
+                            echo 'Tên đăng nhập là: ' . $_POST['username'];
+                            echo '<br/>';
+                            echo 'Mật khẩu là: ' . $_POST['password'];
+                        }
+                    ?>
+                </form>
             </div>
     </body>        
 </html>
